71edffd9cd449515855426365f96b47ef9299957
Hubblle <lubin.tsch@gmail.com>
2025-04-15 18:16:35 +0200

    First test

--- script/main-no-switch.py
+++ script/main-no-switch.py
@@ -7,7 +7,6 @@ CW = 1     # Clockwise Rotation
 CCW = 0    # Counterclockwise Rotation
 SPR = 200   # Steps per Revolution (360 / 1.8)
 EN = 2
-SWITCH = 26
 
 GPIO.setmode(GPIO.BCM)
 
@@ -15,8 +14,6 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(EN, GPIO.OUT)
 GPIO.output(EN, GPIO.LOW)
 
-#Setup the switch up
-GPIO.setup(SWITCH, GPIO.IN)
 
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
@@ -35,28 +32,13 @@ GPIO.output(MODE, RESOLUTION['1/32'])
 step_count = SPR
 delay = .005 / 32
 
-try :
-    while True:
-        print(GPIO.input(SWITCH))
-        if GPIO.input(SWITCH) == GPIO.LOW:
-            GPIO.output(DIR, CW)
-            GPIO.output(STEP, GPIO.HIGH)
-            sleep(delay)
-            GPIO.output(STEP, GPIO.LOW)
-            sleep(delay)
 
-        elif GPIO.input(SWITCH) == GPIO.HIGH :
-            GPIO.output(DIR, CCW)
-            for x in range(step_count*3):
-                GPIO.output(STEP, GPIO.HIGH)
-                sleep(delay)
-                GPIO.output(STEP, GPIO.LOW)
-                sleep(delay)
+for x in range(step_count):
+    GPIO.output(STEP, GPIO.HIGH)
+    sleep(delay)
+    GPIO.output(STEP, GPIO.LOW)
+    sleep(delay)
 
-except :
-    GPIO.output(EN, GPIO.HIGH)
-    GPIO.cleanup()
-    exit()
 
 #stop the board and cleanup
 GPIO.output(EN, GPIO.HIGH)
