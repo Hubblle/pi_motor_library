c03723ff14ef75cd687068bd129011da7694da02
Hubblle <lubin.tsch@gmail.com>
2025-05-03 00:12:14 +0200

    Added new step into the main process, using new func of the motor class.

--- script/main.py
+++ script/main.py
@@ -2,19 +2,19 @@ from time import sleep
 import RPi.GPIO as GPIO
 from motor_lib import Motor
 
-Y_mottor_info = {
+Y_motor_info = {
     "DIR" : 3, # Direction GPIO Pin
     "STEP" : 2, # Step GPIO Pin
     "SWITCH" : 4 # Switch GPIO Pin
 }
 
-X_mottor_info = {
+X_motor_info = {
     "DIR" : 27, # Direction GPIO Pin
     "STEP" : 17, # Step GPIO Pin
     "SWITCH" : 22 # Switch GPIO Pin
 }
 
-Z_mottor_info = {
+Z_motor_info = {
     "DIR" : 9, # Direction GPIO Pin
     "STEP" : 10, # Step GPIO Pin
     "SWITCH" : 11 # Switch GPIO Pin
@@ -25,19 +25,21 @@ CCW = 0    # Counterclockwise Rotation
 SPR = 200   # Steps per Revolution (360 / 1.8)
 EN = 18 # Enable GPIO Pin
 
-Y_mottor = Motor(Y_mottor_info)
-Y_mottor.setup()
+Y_motor = Motor(Y_motor_info, "Y_motor")
+Y_motor.setup()
 
-X_mottor = Motor(X_mottor_info)
+X_motor = Motor(X_motor_info, "X_motor")
 
-Z_mottor = Motor(Z_mottor_info)
+Z_motor = Motor(Z_motor_info, "Z_motor")
 
 
 step_count = SPR * 16
 delay = .004 / 16
 
-Y_mottor.high(SPR)
-Y_mottor.low(SPR)
+Y_motor.high(SPR)
+Y_motor.down(SPR)
+Y_motor.reset()
+Y_motor.down(step_count)
 
 #stop the board and cleanup
 GPIO.output(EN, GPIO.HIGH)
