f7b261692dff2d0c36771bcc4dadd1aad286c351
Hubblle <lubin.tsch@gmail.com>
2025-05-02 19:36:38 +0200

    sync

--- script/main.py
+++ script/main.py
@@ -1,6 +1,7 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
+
 DIR = 20   # Direction GPIO Pin
 STEP = 21  # Step GPIO Pin
 CW = 1     # Clockwise Rotation
@@ -22,14 +23,6 @@ GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
 GPIO.output(DIR, CW)
 
-MODE = (25, 8, 7)   # Microstep Resolution GPIO Pins
-GPIO.setup(MODE, GPIO.OUT)
-RESOLUTION = {'Full': (0, 0, 0),
-              'Half': (1, 0, 0),
-              '1/4': (0, 1, 0),
-              '1/8': (1, 1, 0),
-              '1/16': (1, 1, 1)}
-GPIO.output(MODE, RESOLUTION['1/16'])
 
 step_count = SPR * 16
 delay = .004 / 16
